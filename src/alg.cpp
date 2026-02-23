5a12c18488a24a66bc110a9a703b3e474d7257d3
Lubitel <artzaxa07@gmail.com>
2026-02-23 12:51:21 +0300

    a2

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,12 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value == 2){
+    if (value == 2) {
 		return true;
-	}
-    else {
+	}else {
         uint64_t lim = 0;
         while (lim * lim < value) lim++;
-        for (uint64_t i = 2; i < lim+1; i++){
+        for (uint64_t i = 2; i < lim+1; i++) {
             if (value % i == 0){
                 return false;
             }
@@ -19,7 +18,7 @@ bool checkPrime(uint64_t value) {
     }
 }
 
-uint64_t nPrime(uint64_t n){
+uint64_t nPrime(uint64_t n) {
 	uint64_t i = 0;
 	uint64_t iter = 2;
 	while(1){
@@ -29,7 +28,7 @@ uint64_t nPrime(uint64_t n){
 	}
 }
 
-uint64_t nextPrime(uint64_t value){
+uint64_t nextPrime(uint64_t value) {
 	uint64_t iter = value+1;
 	while (iter < UINT64_MAX){
 		if (checkPrime(iter)) return iter;
@@ -38,7 +37,7 @@ uint64_t nextPrime(uint64_t value){
 	return 0;
 }
 
-uint64_t sumPrime(uint64_t hbound){
+uint64_t sumPrime(uint64_t hbound) {
 	uint64_t sum = 0;
 	for (uint64_t i = 2; i < hbound; i++){
 		if (checkPrime(i))sum += i;
@@ -46,12 +45,12 @@ uint64_t sumPrime(uint64_t hbound){
 	return sum;
 }
 
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound){
-    uint64_t count = 0;
-    for (; lbound < hbound - 2; lbound++){
-        if (checkPrime(lbound) && checkPrime(lbound + 2)){
-            count++;
-        }
-    }
-    return count;
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
+	uint64_t count = 0;
+	for (; lbound < hbound - 2; lbound++) {
+		if (checkPrime(lbound) && checkPrime(lbound + 2)){
+			count++;
+		}
+	}
+	return count;
 }
